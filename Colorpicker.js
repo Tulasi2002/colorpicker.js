8add8a1245f6154c5959de02785180c52b7730b8
Tulasi2002 <111428503+Tulasi2002@users.noreply.github.com>
2024-09-28 01:51:33 -0700

    Add files via upload

---
+++ Colorpicker.js
@@ -0,0 +1,25 @@
+let colorchange = document.getElementById("Background");
+let colortext = document.getElementById("span");
+
+
+
+function colorone() {
+    colorchange.style.backgroundColor = "#e0e0e0";
+    colortext.textContent = "#e0e0e0"
+}
+
+function colortwo() {
+    colorchange.style.backgroundColor = " #6fcf97";
+    colortext.textContent = " #6fcf97"
+}
+
+function colorthree() {
+    colorchange.style.backgroundColor = "#56ccf2";
+    colortext.textContent = "#56ccf2"
+}
+
+function colorfour() {
+    colorchange.style.backgroundColor = "#bd6bd9";
+    colortext.textContent = "#bd6bd9";
+
+}
